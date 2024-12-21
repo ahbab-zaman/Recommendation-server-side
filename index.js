5856ed89c719a2887335deb7ef5c42b8ad801af1
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-12-22 01:25:29 +0600

    deploying the server side

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.REVIEW_DB}:${process.env.REVIEW_PASS}@cluster0.73pqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,11 +20,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
   } finally {
   }
 }
